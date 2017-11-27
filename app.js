e2a974f641caf99a967e884b62253a1e557ae26d
Jared Pleva <Prime@Jareds-MacBook-Pro.local>
2017-11-27 12:38:41 -0700

    ready for aw

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // app.use('/', index);
 // app.use('/users', users);
