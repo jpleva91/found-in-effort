3f966794c7222ae07f46a145af92dd3f2fbc4d62
Jared Pleva <Prime@Jareds-MacBook-Pro.local>
2017-11-27 14:26:40 -0700

    heroku erros

--- app.js
+++ app.js
@@ -15,14 +15,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build/')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+// app.use('/', index);
+// app.use('/users', users);
 // 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -47,8 +49,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-const port = process.env.PORT || 5000;
-app.listen(port);
 
 module.exports = app;
